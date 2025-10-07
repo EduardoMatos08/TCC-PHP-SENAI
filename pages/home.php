528382fc1ff73d722433f263213c4dfc01b6a14e
dinuel9191-a11y <dinuel9191@gmail.com>
2025-10-07 07:38:46 -0300

    Add files via upload

--- pages/home.php
+++ pages/home.php
@@ -26,18 +26,3 @@
 
 </html>
 
-<?php if (!empty($_SESSION['admin']) && $_SESSION['admin'] == 1): ?>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-      Administrador
-    </a>
-    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-      <li><a class="dropdown-item" href="./adm_professores.php">Professores</a></li>
-      <li><a class="dropdown-item" href="./adm_materias.php">Disciplinas</a></li>
-      <li>
-        <hr class="dropdown-divider">
-      </li>
-      <li><a class="dropdown-item" href="#">Horários</a></li>
-    </ul>
-  </li>
-<?php endif; ?>
