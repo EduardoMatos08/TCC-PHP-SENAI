d12116dd6ae21532a87e877e6a30dc5fe761bf2c
dinuel9191-a11y <dinuel9191@gmail.com>
2025-10-07 07:38:04 -0300

    home não está mais bugando

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<style>
+  .navbar-text {
+    font-weight: 500;
+    font-size: 18px;
+  }
+</style>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Lançadeiro Senai</title>
+  <!-- Importação dos Scripts e Estilos - Status: Funcionando -->
+  <link rel="stylesheet" href="../bootstrap-styles/bootstrap.css" />
+  <script src="../bootstrap-styles/bootstrap.js"></script>
+  <link rel="icon" type="image/x-icon" href="../assets/favicon.png">
+</head>
+
+<body>
+  <!-- Corpo do Site -->
+  <!-- Navbar -->
+  <?php include '../components/navbar.php'; ?>
+</body>
+
+</html>
+
